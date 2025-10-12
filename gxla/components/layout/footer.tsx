e7bed1b1a5100d1fc1a91528c42f46c227210e5d
Ruben Yanez <rubenyanezhernandez@gmail.com>
2025-10-11 19:35:18 -0700

    feat(ui): complete navbar and footer (#33)
    * feat(ui): complete navbar and footer
    
    * refactor(layout): wrap Image and ul with div and adjust footer and header gap variable
    
    Updated layout structure for better alignment and spacing.
    - Wrapped Image and ul with div in header component to use gap.
    - Adjusted footer and header gap variable to maintain consistent spacing
    
    ---------
    
    Co-authored-by: EmilioFex20 <emilio.pouget@cetys.edu.mx>

--- gxla/components/layout/footer.tsx
+++ gxla/components/layout/footer.tsx
@@ -1,6 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export function Footer(){
     return(
         <>
+        <div className="flex flex-col md:flex-row justify-center lg:gap-[20%] md:gap-[12%] sm:gap-8 bg-[#1E2939] text-white px-10 py-10">
+            <div className="md:w-1/3">
+                <Image 
+                src="/GXLA_Logo.png" 
+                alt="Logo"
+                width={195}
+                height={74}
+                />
+                <p className="mt-3">
+                    Nuestra misión es promover el bienestar animal mediante los programas de esterilización, adopción, 
+                    educación y denuncia ciudadana; e incidir en las políticas públicas que determinen las autoridades 
+                    para el control ético de la población canina y felina.
+                </p>
+            </div>
+
+            <div>
+                <h1 className="font-bold text-3xl mb-2"> Contacto </h1>
+                <p>genteporlosanimales@gmail.com</p>
+            </div>
+
+            <div>
+                <h1 className="font-bold text-3xl mb-2"> Siguenos </h1>
+                <div className="flex gap-6.5">
+                    <Link href="" target="_blank" rel="noopener noreferrer" className="cursor-pointer">
+                        <Image 
+                        src="/redes/facebook.png" 
+                        alt="Facebook"
+                        width={55}
+                        height={55}
+                        />
+                    </Link>
+                    <Link href="" target="_blank" rel="noopener noreferrer" className="cursor-pointer">
+                        <Image 
+                        src="/redes/insta.png" 
+                        alt="Instagram"
+                        width={55}
+                        height={55}
+                        />
+                    </Link>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
--- gxla/components/layout/header.tsx
+++ gxla/components/layout/header.tsx
@@ -1,6 +1,117 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useState } from "react";
+
 export function Header(){
+    const [isOpen, setIsOpen] = useState(false);
     return(
-        <>
-        </>
+        <nav className="fixed top-0 left-0 w-screen bg-white backdrop-blur-md z-1000 shadow-sm shadow-[#00A63E] text-black">
+            <div className="flex items-center justify-between h-full px-2 py-2 mx-5 my-4 gap-10">
+                <div>
+                    <Image 
+                        src="/GXLA_Logo.png" 
+                        alt="Logo"
+                        width={164}
+                        height={74}
+                    />
+                </div>
+            <div>
+                <ul className="hidden md:flex justify-center space-x-5 lg:gap-5 md:gap-2 items-center">
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/">Inicio</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/nosotros">¿Quienes somos?</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="#Contacto">Programas</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/difusion">Difusión</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/acciones">¿Cómo ayudar?</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="#Contacto">Contacto</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] bg-[#06F180] shadow-lg py-2 px-4 font-bold rounded-2xl transition-all hover:scale-105 cursor-pointer">
+                        <Link href="#Donar">Donar</Link>
+                    </li>
+                </ul>
+            </div>
+            
+
+            {!isOpen && (
+
+            <button 
+            className="md:hidden text-3xl cursor-pointer text-[#00A63E] hover:scale-110 transition" 
+            onClick={() => setIsOpen(!isOpen)}
+            >
+            ☰
+            </button>
+            )}
+            {isOpen && (
+            <button 
+            className="md:hidden text-3xl cursor-pointer text-[#00A63E] hover:scale-110 transition" 
+                onClick={() => setIsOpen(false)}
+            >
+                ✖
+            </button>
+            )}
+            {isOpen && (
+            <aside className="absolute top-full left-0 w-full bg-white shadow-md shadow-[#00A63E] px-4 py-4">
+
+                <ul className="flex flex-col space-y-4 p-5">
+                    <li
+                        className="text-[18px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] hover:scale-102 transition-all duration-300 cursor-pointer"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Link href="/">Inicio</Link>
+                    </li>
+                    <li
+                        className="text-[18px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] hover:scale-102 transition-all duration-300 cursor-pointer"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Link href="/nosotros">¿Quienes somos?</Link>
+                    </li>
+                    <li
+                        className="text-[18px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] hover:scale-102 transition-all duration-300 cursor-pointer"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Link href="#Contacto">Programas</Link>
+                    </li>
+                    <li
+                        className="text-[18px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] hover:scale-102 transition-all duration-300 cursor-pointer"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Link href="/difusion">Difusión</Link>
+                    </li>
+                    <li
+                        className="text-[18px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] hover:scale-102 transition-all duration-300 cursor-pointer"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Link href="/acciones">¿Cómo ayudar?</Link>
+                    </li>
+                    <li
+                        className="text-[18px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] hover:scale-102 transition-all duration-300 cursor-pointer"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Link href="#Contacto">Contacto</Link>
+                    </li>
+                    <li
+                        className="text-[18px] bg-[#06F180] py-2 px-4 font-bold rounded-2xl transition-all cursor-pointer hover:scale-102 text-center shadow-lg"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Link href="#Donar">Donar</Link>
+                    </li>
+                </ul>
+            </aside>
+            )}
+            </div>
+        </nav>
     );
 }
