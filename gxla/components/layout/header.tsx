48edd37231d6516a2fab337f4b51360f9a9e0fa3
EmilioFex20 <emilio.pouget@cetys.edu.mx>
2025-10-11 19:31:28 -0700

    refactor(layout): wrap Image and ul with div and adjust footer and header gap variable
    Updated layout structure for better alignment and spacing.
    - Wrapped Image and ul with div in header component to use gap.
    - Adjusted footer and header gap variable to maintain consistent spacing

--- gxla/components/layout/header.tsx
+++ gxla/components/layout/header.tsx
@@ -9,36 +9,41 @@ export function Header(){
     const [isOpen, setIsOpen] = useState(false);
     return(
         <nav className="fixed top-0 left-0 w-screen bg-white backdrop-blur-md z-1000 shadow-sm shadow-[#00A63E] text-black">
-            <div className="flex items-center justify-between h-full px-4 py-2 mx-5 md:mx-12 lg:mx-50 my-4">
-            <Image 
-            src="/GXLA_Logo.png" 
-            alt="Logo"
-            width={164}
-            height={74}
-            />
-            <ul className="hidden md:flex justify-end space-x-5 gap-5 items-center">
-                <li className="text-[20px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
-                    <Link href="/">Inicio</Link>
-                </li>
-                <li className="text-[20px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
-                    <Link href="/nosotros">¿Quienes somos?</Link>
-                </li>
-                <li className="text-[20px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
-                    <Link href="#Contacto">Programas</Link>
-                </li>
-                <li className="text-[20px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
-                    <Link href="/difusion">Difusión</Link>
-                </li>
-                <li className="text-[20px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
-                    <Link href="/acciones">¿Cómo ayudar?</Link>
-                </li>
-                <li className="text-[20px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
-                    <Link href="#Contacto">Contacto</Link>
-                </li>
-                <li className="text-[20px] bg-[#06F180] shadow-lg py-2 px-4 font-bold rounded-2xl transition-all hover:scale-105 cursor-pointer">
-                    <Link href="#Donar">Donar</Link>
-                </li>
-            </ul>
+            <div className="flex items-center justify-between h-full px-2 py-2 mx-5 my-4 gap-10">
+                <div>
+                    <Image 
+                        src="/GXLA_Logo.png" 
+                        alt="Logo"
+                        width={164}
+                        height={74}
+                    />
+                </div>
+            <div>
+                <ul className="hidden md:flex justify-center space-x-5 lg:gap-5 md:gap-2 items-center">
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/">Inicio</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/nosotros">¿Quienes somos?</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="#Contacto">Programas</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/difusion">Difusión</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="/acciones">¿Cómo ayudar?</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] hover:underline hover:underline-offset-4 hover:decoration-[#06F180] transition-all duration-300 hover:scale-105 origin-center">
+                        <Link href="#Contacto">Contacto</Link>
+                    </li>
+                    <li className="lg:text-[20px] md:text-[14px] bg-[#06F180] shadow-lg py-2 px-4 font-bold rounded-2xl transition-all hover:scale-105 cursor-pointer">
+                        <Link href="#Donar">Donar</Link>
+                    </li>
+                </ul>
+            </div>
+            
 
             {!isOpen && (
 
