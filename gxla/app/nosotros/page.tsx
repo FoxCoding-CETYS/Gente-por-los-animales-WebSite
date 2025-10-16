d576c5c4176a3d052573df4206873f0522800be2
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2025-10-16 16:30:35 -0700

    feat(ui): finished large layout, small layout missing

--- gxla/app/nosotros/page.tsx
+++ gxla/app/nosotros/page.tsx
@@ -5,13 +5,13 @@ export default function NosotrosPage() {
     return(
         <>
             {/* Hero Section */}
-            <div className="flex flex-col items-center justify-center py-32 px-3 mt-12 w-screen bg-[#05DF72] text-black text-center">
-                <h1 className="font-geist text-black text-6xl font-bold mt-10 md:text-[50px]"> ¿Quiénes somos? </h1>
-                <p className="text-3xl"> Conoce nuestra historia, misión y los valores que nos impulsan a trabajar por el bienestar animal. </p>
+            <div className="flex flex-col items-center justify-center lg:py-24 py-24 px-3 mt-12 w-screen lg:bg-[#05DF72] md:bg-amber-200 bg-amber-600 text-black text-center gap-6">
+                <h1 className="font-geist text-black lg:text-[64px] font-bold md:text-[50px] text-[32px]"> ¿Quiénes somos? </h1>
+                <p className="lg:text-3xl lg:w-7/12 w-11/12 text-[20px]"> Conoce nuestra historia, misión y los valores que nos impulsan a trabajar por el bienestar animal. </p>
             </div>
             {/* Historia */}
             <div className="w-screen flex flex-col items-center text-center px-4 gap-4">
-                <h2 className="pt-10 font-bold lg:text-[36px] sm:text-[24px]">Historia</h2>
+                <h2 className="pt-10 font-bold lg:text-[36px] text-[32px]">Historia</h2>
                 <p className="lg:w-[1161px] sm:w-[270px] lg:text-[20px] sm:text-[14px]">
                     Para muchas personas, el primer contacto consciente con el reino animal es a través de los perros y gatos con los que en algún momento convivimos. Lamentablemente, se ha vuelto común verlos en las calles, sin supervisión, hambrientos, enfermos o atropellados, exhibiendo nuestra falta de responsabilidad y poniendo a prueba la indiferencia de la gente.<br/><br/>
                     Gente por los Animales, A.C. surgió como un esfuerzo para contribuir con la protección del ambiente desde la trinchera del bienestar animal, y más específicamente, de los animales de compañía.
@@ -20,34 +20,36 @@ export default function NosotrosPage() {
                 <CustomButton size="lg" className="mb-14 mt-6">LEER HISTORIA COMPLETA</CustomButton>
             </div>
             {/* Mision y vision */}
-            <div className=" flex bg-[#F9FAFB] w-screen flex-row justify-center gap-10 lg:py-20 sm:py-10 lg:flex-row sm:flex-col">
-                <CustomCard variant="normal" size="lg" icon="/Circle.png" title="Misión">Promover el bienestar animal mediante los programas de esterilización, adopción, educación y denuncia ciudadana; e incidir en las políticas públicas que determinen las autoridades para el control ético de la población canina y felina.</CustomCard>
-                <CustomCard variant="normal" size="lg" icon="/Circle.png" title="Visión">Ser una asociación civil profesional, activa e innovadora, que contribuya al bienestar de la sociedad mediante la implementación de programas integrales en Mexicali que fomenten el bienestar animal.</CustomCard>
-            </div>
-            {/* Valores */}
-            <div className=" flex bg-[#F9FAFB] w-screen flex-row justify-center gap-10 lg:py-20 sm:py-10 lg:flex-row sm:flex-col">
-                <CustomCard variant="big"
-                title1="Participación"
-                text1="Aspiramos al involucramiento de la sociedad civil en estrategias que contribuyan a resolver problemáticas comunes."
-                title2="Respeto"
-                text2="Animamos a conducirse de manera considerada, con independencia de las diferencias que puedan llegar a surgir entre las personas."
-                title3="Compromiso"
-                text3="Edificamos nuestra labor sobre el compromiso individual hacia la causa, el equipo de trabajo, y los usuarios de nuestros servicios."
-                title4="Responsabilidad"
-                text4="Fomentamos que los voluntarios cumplan con sus obligaciones, enfrentando con inteligencia las situaciones que se le presenten."
-                title5="Compasión"
-                text5="Procuramos colocarnos en el lugar del otro, para entender sus necesidades y ayudarlo en la medida de nuestras posibilidades."
-                title6="Apertura"
-                text6="Propiciamos la consideración de ideas diversas, para el enriquecimiento de las propias."
-                icon="/heart.png"
-                title="Nuestros valores"
-                ></CustomCard>
+            <div className=" flex bg-[#F9FAFB] w-screen flex-col justify-center">
+                <div className="flex lg:flex-row justify-center items-center gap-12 lg:mt-20 flex-col mt-12">
+                    <CustomCard variant="normal" size="lg" icon="/Circle.png" title="Misión">Promover el bienestar animal mediante los programas de esterilización, adopción, educación y denuncia ciudadana; e incidir en las políticas públicas que determinen las autoridades para el control ético de la población canina y felina.</CustomCard>
+                    <CustomCard variant="normal" size="lg" icon="/Circle.png" title="Visión">Ser una asociación civil profesional, activa e innovadora, que contribuya al bienestar de la sociedad mediante la implementación de programas integrales en Mexicali que fomenten el bienestar animal.</CustomCard>
+                </div>
+                {/* Valores */}
+                <div className=" flex bg-[#F9FAFB] w-screen flex-row justify-center gap-10 lg:py-20 sm:py-10 lg:flex-row sm:flex-col">
+                    <CustomCard variant="big"
+                    title1="Participación"
+                    text1="Aspiramos al involucramiento de la sociedad civil en estrategias que contribuyan a resolver problemáticas comunes."
+                    title2="Respeto"
+                    text2="Animamos a conducirse de manera considerada, con independencia de las diferencias que puedan llegar a surgir entre las personas."
+                    title3="Compromiso"
+                    text3="Edificamos nuestra labor sobre el compromiso individual hacia la causa, el equipo de trabajo, y los usuarios de nuestros servicios."
+                    title4="Responsabilidad"
+                    text4="Fomentamos que los voluntarios cumplan con sus obligaciones, enfrentando con inteligencia las situaciones que se le presenten."
+                    title5="Compasión"
+                    text5="Procuramos colocarnos en el lugar del otro, para entender sus necesidades y ayudarlo en la medida de nuestras posibilidades."
+                    title6="Apertura"
+                    text6="Propiciamos la consideración de ideas diversas, para el enriquecimiento de las propias."
+                    icon="/heart.png"
+                    title="Nuestros valores"
+                    ></CustomCard>
+                </div>
             </div>
             {/* Objetivos */}
             <div className="flex flex-col justify-center items-center">
                 <h1 className="font-geist text-black text-6xl font-bold mt-10 md:text-[50px]"> Objetivos </h1>
-                <div className="flex flex-row">
-                    <ul className="list-disc">
+                <div className="flex flex-row mr-24 ml-24 mt-8 mb-8 gap-20">
+                    <ul className="list-disc w-8/12">
                         <li>Reubicar con familias responsables y cariñosas, a través del programa de adopción, a aquellos animales que han sido rescatados de la calle, abandonados, maltratados y/o que otras personas ya no pueden cuidar.</li>
                         <li>Incrementar la cantidad de perros y gatos esterilizados.</li>
                         <li>Concientizar sobre las necesidades de los animales de compañía y el respeto hacia los mismos.</li>
@@ -55,7 +57,7 @@ export default function NosotrosPage() {
                         <li>Promover reglas básicas para una convivencia respetuosa entre los guardianes de animales y el resto de la sociedad.</li>
                         <li>Supervisar la aplicación del marco jurídico relacionado con el bienestar animal.</li>
                     </ul>
-                    <ul className="list-disc">
+                    <ul className="list-disc w-8/12">
                         <li>Impulsar la utilización de métodos humanitarios para la captura y la matanza de animales de compañía.</li>
                         <li>Incidir en políticas públicas para el control ético de la población canina y felina.</li>
                         <li>Participar con las autoridades de los distintos órdenes de gobierno, competentes para atender asuntos de bienestar animal.</li>
@@ -101,7 +103,6 @@ export default function NosotrosPage() {
                     <CustomCard variant="normal" size="lg" icon="/heartblue.png" title="Cuotas de recuperación" className="w-11/12">
                         <p className="text-left">
                             Al prestar algunos de los servicios de la organización, se requieren cuotas de recuperación que ayuden a solventar en todo o en parte el costo que esto representa.
-                        <br /><br />
                             Por ejemplo, al adoptar un perro o un gato, se solicita una cuota de recuperación que, si bien no solventa en su totalidad la inversión que la asociación realiza en cada uno de los animales, resulta de ayuda para compensar una parte.
                         </p>
                     </CustomCard>
@@ -116,9 +117,9 @@ export default function NosotrosPage() {
             </div>
             {/* Call to action */}
             <div className="flex flex-col items-center justify-center px-3 mt-12 w-screen bg-[#05DF72] text-black text-center">
-                <h1 className="font-geist text-black text-3xl font-bold mt-10 md:text-[36px]"> ¿Quiéres ser parte del cambio? </h1>
-                <p className="text-[20px]"> Conoce nuestra historia, misión y los valores que nos impulsan a trabajar por el bienestar animal. </p>
-                <CustomButton size="lg" className="mb-14 mt-6">QUIERO AYUDAR</CustomButton>
+                <h1 className="font-geist text-black text-3xl font-bold mt-10 md:text-[36px] p-4"> ¿Quiéres ser parte del cambio? </h1>
+                <p className="text-[20px] p-5"> Únete a nuestra misión de promover el bienestar animal en nuestra comunidad.</p>
+                <CustomButton size="lg" className="mb-14 mt-6" variant="secondary">Voluntariado</CustomButton>
             </div>
         </>
     );
--- gxla/components/ui/custom-card.tsx
+++ gxla/components/ui/custom-card.tsx
@@ -27,7 +27,7 @@ interface CustomCardProps {
 const sizeClasses = {
   sm: "px-4 py-2 text-sm",
   md: "lg:h-[354px] lg:w-[288px]",
-  lg: "lg:w-[545px]",
+  lg: "lg:w-[545px] w-7/11",
 }
 
 export function CustomCard({
@@ -57,19 +57,19 @@ export function CustomCard({
         className={`flex flex-col items-center text-center shadow-md rounded-[10px] border border-gray-200 bg-white ${sizeClasses[size]} ${className}`}
         onClick={onClick}
       >
-        {icon && <img src={icon} alt="" className="mb-2" />}
-        {title && <h3 className="text-[32px] font-bold">{title}</h3>}
-        {children && <p className="text-[20px] w-11/12 pt-[20px] font-light mb-5">{children}</p>}
+        {icon && <img src={icon} alt="" className="mb-2 w-16 h-16 mt-3" />}
+        {title && <h3 className="lg:text-[32px] text-[24px] font-bold">{title}</h3>}
+        {children && <div className="lg:text-[20px] w-11/12 text-[14px] pt-[20px] font-light mb-5">{children}</div>}
       </div>
     )
   }
   else{
   return (
     <div
-      className={`flex flex-col items-center justify-center text-center shadow-md rounded-[10px] border border-gray-200 bg-white lg:w-10/12 ${className}`}
+      className={`flex flex-col items-center justify-center text-center shadow-md rounded-[10px] border border-gray-200 bg-white w-10/12 ${className}`}
       onClick={onClick}
     >
-      {icon && <img src={icon} alt="" className="mb-2" />}
+      {icon && <img src={icon} alt="" className="mb-2 w-20 h-20 mt-3" />}
       {title && <h3 className="text-[36px] font-bold">{title}</h3>} 
       <div className="w-[1173px]">
         <div className="flex flex-row">
@@ -78,9 +78,9 @@ export function CustomCard({
           {title3 && <h1 className="w-[454px] pt-[20px] font-bold text-[32px]">{title3}</h1>}
         </div>
         <div className="flex flex-row">
-          {text1 && <p className="text-[20px] w-[454px] pt-[20px] font-light">{text1}</p>}
-          {text2 && <p className="text-[20px] w-[454px] pt-[20px] font-light">{text2}</p>}
-          {text3 && <p className="text-[20px] w-[454px] pt-[20px] font-light">{text3}</p>}
+          {text1 && <p className="text-[20px] w-8/12 pt-[20px] font-light">{text1}</p>}
+          {text2 && <p className="text-[20px] w-8/12 pt-[20px] font-light">{text2}</p>}
+          {text3 && <p className="text-[20px] w-8/12 pt-[20px] font-light">{text3}</p>}
         </div>
         <div className="flex flex-row">
           {title4 && <h1 className="text-[32px] w-[454px] pt-[20px] font-bold">{title4}</h1>}
