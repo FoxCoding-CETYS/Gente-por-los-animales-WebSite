5e0ba170463917e5864d4c13ea5238c87956101e
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2025-10-24 15:57:43 -0700

    feat(ui): finished about us page:

--- gxla/app/nosotros/page.tsx
+++ gxla/app/nosotros/page.tsx
@@ -9,28 +9,28 @@ export default function NosotrosPage() {
     const HistoriaCompleta = () => {
         setCompleto(!completo);
     }
+
     return(
         <>
             {/* Hero Section */}
-            <div className="flex flex-col items-center justify-center lg:py-24 py-24 px-3 w-screen lg:bg-[#05DF72] md:bg-amber-200 bg-amber-600 text-black text-center gap-6">
+            <div className="flex flex-col items-center justify-center lg:py-16 py-8 w-screen bg-[#05DF72] text-black text-center gap-6">
                 <h1 className="font-geist text-black lg:text-[64px] font-bold md:text-[50px] text-[32px]"> ¿QUIÉNES SOMOS? </h1>
                 <p className="lg:text-3xl lg:w-6/12 w-11/12 text-[20px]"> Conoce nuestra historia, misión y los valores que nos impulsan a trabajar por el bienestar animal. </p>
             </div>
             {/* Historia */}
             <div className="w-screen flex flex-col items-center text-center px-4 gap-4">
                 <h2 className="pt-10 font-bold lg:text-[36px] text-[32px]">HISTORIA</h2>
-                <p className="lg:w-9/12 lg:text-[20px] sm:text-[14px] text-left leading-9">
+                <p className="lg:w-9/12 lg:text-[20px] text-[16px] text-left leading-9">
                     Para muchas personas, el primer contacto consciente con el reino animal es a través de los perros y gatos con los que en algún momento convivimos. Lamentablemente, se ha vuelto común verlos en las calles, sin supervisión, hambrientos, enfermos o atropellados, exhibiendo nuestra falta de responsabilidad y poniendo a prueba la indiferencia de la gente.<br/><br/>
                     Gente por los Animales, A.C. surgió como un esfuerzo para contribuir con la protección del ambiente desde la trinchera del bienestar animal, y más específicamente, de los animales de compañía.
                 </p>
-                { completo && <p className="lg:w-9/12 lg:text-[20px] sm:text-[14px] text-left leading-9">La apertura del Centro Municipal de Control Animal de Mexicali en el año 2006, detonó nuestra intención de hacer algo, obligándonos a actuar para brindarles una segunda oportunidad a estos animales, que de otra manera serían capturados y sacrificados. <br /><br />
+                { completo && <p className="lg:w-9/12 lg:text-[20px] text-[16px] text-left leading-9">La apertura del Centro Municipal de Control Animal de Mexicali en el año 2006, detonó nuestra intención de hacer algo, obligándonos a actuar para brindarles una segunda oportunidad a estos animales, que de otra manera serían capturados y sacrificados. <br /><br />
                                 A través de un correo electrónico, nos pusimos en contacto varias personas con una visión similar sobre la forma de trabajo para ayudar a estos animales. Así, Patricia Torres e Ivonne Cannett, definieron en primera instancia el que sería nuestro programa de adopciones, basado en un sistema de casas puente y hogares temporales. <br /><br />
                                 Dos perros rescatados del Centro Municipal de Control Animal – Stuart y Brandy – fueron los primeros animales ingresados al programa de adopción. Sus fotos e historias se publicaron en la primera página de internet, elaborada por Alberto Mexía. <br /><br />
                                 El 28 de julio de 2006 se publicó una nota en un importante diario de nuestra comunidad, en la cual se expuso brevemente el propósito del programa de adopciones de Gente por los Animales. A raíz del mismo, varias personas se comunicaron con la intención de participar. <br /><br />
                                 Tras casi un año de actividades y con grupo más consolidado, el 5 de junio del año 2007 firmamos el acta constitutiva mediante la cual Gente por los Animales adquirió personalidad jurídica como una asociación civil sin fines de lucro. <br /><br />
                                 <b>Fundadores: Patricia Torres, Yazmin Valdez, Isela Landeros, Yissel Vidal, Ivonne Cannet, Thelma Gutiérrez, Claudia Mora, Karina Cázarez, Paloma Ponce, Julio Guerrero, Brenda López, David Esparza, Michelle Esparza y Mónica Acosta.</b><br /><br />
                             En abril del 2009 Gente por los Animales fue reconocido con el Premio UVM por el Desarrollo Social auspiciado por la International Youth Foundation. Un año después nos convertimos en donataria autorizada por el SAT. <br /><br /></p> }
-                {/* Cambiar el color del boton */}
                 <CustomButton size="lg" className="mb-14 mt-6 shadow-lg" variant="outline" onClick={HistoriaCompleta}>LEER HISTORIA COMPLETA</CustomButton>
             </div>
             {/* Mision y vision */}
@@ -40,7 +40,7 @@ export default function NosotrosPage() {
                     <CustomCard variant="normal" size="lg" icon="/Circle.png" title="VISIÓN">Ser una asociación civil profesional, activa e innovadora, que contribuya al bienestar de la sociedad mediante la implementación de programas integrales en Mexicali que fomenten el bienestar animal.</CustomCard>
                 </div>
                 {/* Valores */}
-                <div className=" flex bg-[#F9FAFB] w-screen flex-row justify-center gap-10 lg:py-20 sm:py-10 lg:flex-row sm:flex-col">
+                <div className=" flex bg-[#F9FAFB] w-screen flex-row justify-center items-center gap-10 lg:py-20 sm:py-10 lg:flex-row sm:flex-col">
                     <CustomCard variant="big"
                     title1="Participación"
                     text1="Aspiramos al involucramiento de la sociedad civil en estrategias que contribuyan a resolver problemáticas comunes."
@@ -61,66 +61,66 @@ export default function NosotrosPage() {
             </div>
             {/* Objetivos */}
             <div className="flex flex-col justify-center items-center">
-                <h1 className="font-geist text-black text-6xl font-bold mt-10 md:text-[50px]"> OBJETIVOS </h1>
-                <div className="flex flex-row mr-24 ml-24 mt-8 mb-8 gap-20 w-10/12">
-                    <ul className="list-disc w-8/12">
-                        <li className="text-[20px]">Reubicar con familias responsables y cariñosas, a través del programa de adopción, a aquellos animales que han sido rescatados de la calle, abandonados, maltratados y/o que otras personas ya no pueden cuidar.</li>
-                        <li className="text-[20px]">Incrementar la cantidad de perros y gatos esterilizados.</li>
-                        <li className="text-[20px]">Concientizar sobre las necesidades de los animales de compañía y el respeto hacia los mismos.</li>
-                        <li className="text-[20px]">Educar sobre bienestar animal y la importancia de la esterilización de los animales de compañía.</li>
-                        <li className="text-[20px]">Promover reglas básicas para una convivencia respetuosa entre los guardianes de animales y el resto de la sociedad.</li>
-                        <li className="text-[20px]">Supervisar la aplicación del marco jurídico relacionado con el bienestar animal.</li>
+                <h1 className="font-geist text-black lg:text-6xl font-bold mt-10 text-[24px]"> OBJETIVOS </h1>
+                <div className="grid lg:grid-cols-2 grid-cols-1 justify-items-center mt-8 mb-5 px-6">
+                    <ul className="list-disc w-10/12">
+                        <li className="lg:text-[20px] text-[16px]">Reubicar con familias responsables y cariñosas, a través del programa de adopción, a aquellos animales que han sido rescatados de la calle, abandonados, maltratados y/o que otras personas ya no pueden cuidar.</li><br />
+                        <li className="lg:text-[20px] text-[16px]">Incrementar la cantidad de perros y gatos esterilizados.</li><br />
+                        <li className="lg:text-[20px] text-[16px]">Concientizar sobre las necesidades de los animales de compañía y el respeto hacia los mismos.</li><br />
+                        <li className="lg:text-[20px] text-[16px]">Educar sobre bienestar animal y la importancia de la esterilización de los animales de compañía.</li><br />
+                        <li className="lg:text-[20px] text-[16px]">Promover reglas básicas para una convivencia respetuosa entre los guardianes de animales y el resto de la sociedad.</li><br />
+                        <li className="lg:text-[20px] text-[16px]">Supervisar la aplicación del marco jurídico relacionado con el bienestar animal.</li><br />
                     </ul>
-                    <ul className="list-disc w-8/12">
-                        <li className="text-[20px]">Impulsar la utilización de métodos humanitarios para la captura y la matanza de animales de compañía.</li>
-                        <li className="text-[20px]">Incidir en políticas públicas para el control ético de la población canina y felina.</li>
-                        <li className="text-[20px]">Participar con las autoridades de los distintos órdenes de gobierno, competentes para atender asuntos de bienestar animal.</li>
-                        <li className="text-[20px]">Fomentar la participación ciudadana en temas relacionados con el bienestar animal.</li>
-                        <li className="text-[20px]">Vincularse con instituciones académicas, asociaciones de profesionistas y el sector empresarial.</li>
-                        <li className="text-[20px]">Colaborar con organismos de la sociedad civil que promuevan valores similares a los nuestros.</li>
+                    <ul className="list-disc w-10/12">
+                        <li className="lg:text-[20px] text-[16px]">Impulsar la utilización de métodos humanitarios para la captura y la matanza de animales de compañía.</li><br />
+                        <li className="lg:text-[20px] text-[16px]">Incidir en políticas públicas para el control ético de la población canina y felina.</li><br />
+                        <li className="lg:text-[20px] text-[16px]">Participar con las autoridades de los distintos órdenes de gobierno, competentes para atender asuntos de bienestar animal.</li><br />
+                        <li className="lg:text-[20px]">Fomentar la participación ciudadana en temas relacionados con el bienestar animal.</li><br />
+                        <li className="lg:text-[20px]">Vincularse con instituciones académicas, asociaciones de profesionistas y el sector empresarial.</li><br />
+                        <li className="lg:text-[20px]">Colaborar con organismos de la sociedad civil que promuevan valores similares a los nuestros.</li><br />
                     </ul>
                 </div>
             </div>
             {/* Alianzas estrategicas */}
             <div className="bg-[#F9FAFB] flex flex-col justify-center items-center">
-                <h1 className="font-geist text-black text-6xl font-bold mt-10 md:text-[36px] text-center"> ALIANZAS ESTRATÉGICAS </h1>
-                <div className="grid grid-cols-2 lg:grid-cols-4 p-8 gap-20 justify-center">
-                    <CustomCard variant="normal" size="md" icon="/Circle.png" title="Clínica y Estética Canina">Atención médica para animales en adopción y programas de esterilización</CustomCard>
-                    <CustomCard variant="normal" size="md" icon="/Circle.png" title="Pet Zone">Clínica veterinaria participante en programas de esterilización</CustomCard>
-                    <CustomCard variant="normal" size="md" icon="/Circle.png" title="DIFOSA">Facilita almacén para resguardo y distribución de donativos</CustomCard>
-                    <CustomCard variant="normal" size="md" icon="/Circle.png" title="Spay México">Coorganizadora de campañas de esterilización</CustomCard>
+                <h1 className="font-geist text-black lg:text-6xl font-bold mt-10 text-[24px] text-center"> ALIANZAS ESTRATÉGICAS </h1>
+                <div className="grid grid-cols-2 lg:grid-cols-4 p-8 gap-5 lg:gap-15 justify-center">
+                    <CustomCard variant="vertical" size="md" icon="/Circle.png" title="Clínica y Estética Canina">Atención médica para animales en adopción y programas de esterilización</CustomCard>
+                    <CustomCard variant="vertical" size="md" icon="/Circle.png" title="Pet Zone">Clínica veterinaria participante en programas de esterilización</CustomCard>
+                    <CustomCard variant="vertical" size="md" icon="/Circle.png" title="DIFOSA">Facilita almacén para resguardo y distribución de donativos</CustomCard>
+                    <CustomCard variant="vertical" size="md" icon="/Circle.png" title="Spay México">Coorganizadora de campañas de esterilización</CustomCard>
                 </div>
             </div>
             {/* Patrocinadores */}
             <div className="flex flex-col justify-center items-center">
-                <h1 className="font-geist text-black text-6xl font-bold mt-10 text-[36px]"> PATROCINADORES </h1>
-                <div className="flex flex-row p-8 gap-10 mt-2 mb-8">
+                <h1 className="font-geist text-black text-6xl font-bold mt-10 text-[24px]"> PATROCINADORES </h1>
+                <div className="grid grid-cols-2 lg:grid-cols-4 p-8 gap-10 mt-2 mb-8">
                     <div >
-                        <img src="/bunker.png" alt="El bunker colectivo" className="w-44 h-44"/>
+                        <img src="/bunker.png" alt="El bunker colectivo" className="lg:w-44 lg:h-44 w-35 h-35"/>
                     </div>
-                    <div className="w-44 h-44 bg-[#F2F2F2] flex items-center justify-center">
-                        <p className="font-bold text-[32px] text-center">Zayda Torres</p>
+                    <div className="lg:w-44 lg:h-44 w-35 h-35 bg-[#F2F2F2] flex items-center justify-center">
+                        <p className="font-bold lg:text-[32px] text-[20px] text-center">Zayda Torres</p>
                     </div>
-                    <div className="w-44 h-44 bg-[#F2F2F2] flex items-center justify-center">
-                        <p className="font-bold text-[32px] text-center">La Casa de Lolo Café & Matcha</p>
+                    <div className="lg:w-44 lg:h-44 w-35 h-35 bg-[#F2F2F2] flex items-center justify-center">
+                        <p className="font-bold lg:text-[32px] text-[20px] text-center">La Casa de Lolo Café & Matcha</p>
                     </div>
-                    <div className="w-44 h-44 bg-[#F2F2F2] flex items-center justify-center">
-                        <p className="font-bold text-[32px] text-center">Mega Homes</p>
+                    <div className="lg:w-44 lg:h-44 w-35 h-35 bg-[#F2F2F2] flex items-center justify-center">
+                        <p className="font-bold lg:text-[32px] text-[20px] text-center">Mega Homes</p>
                     </div>
                 </div>
             </div>
             {/* Financiamiento */}
             <div className="bg-[#F9FAFB] flex flex-col justify-center items-center">
-                <h1 className="font-geist text-black text-6xl font-bold p-10 md:text-[36px]"> FINANCIAMIENTO </h1>
-                <div className="flex flex-row justify-center gap-10">
-                    <CustomCard variant="normal" size="lg" icon="/heartblue.png" title="CUOTAS DE RECUPERACIÓN" className="w-11/12">
+                <h1 className="font-geist text-black text-6xl font-bold p-10 text-[24px]"> FINANCIAMIENTO </h1>
+                <div className="flex lg:flex-row flex-col justify-center gap-10 items-center">
+                    <CustomCard variant="vertical" size="lg" icon="/heartblue.png" title="CUOTAS DE RECUPERACIÓN" className="w-11/12">
                         <p className="text-left">
                             Al prestar algunos de los servicios de la organización, se requieren cuotas de recuperación que ayuden a solventar en todo o en parte el costo que esto representa.
                             <br /><br />
                             Por ejemplo, al adoptar un perro o un gato, se solicita una cuota de recuperación que, si bien no solventa en su totalidad la inversión que la asociación realiza en cada uno de los animales, resulta de ayuda para compensar una parte.
                         </p>
                     </CustomCard>
-                    <CustomCard variant="normal" size="lg" icon="/heartblue.png" title="DONATIVOS" className="w-11/12">
+                    <CustomCard variant="vertical" size="lg" icon="/heartblue.png" title="DONATIVOS" className="w-11/12">
                         <p className="text-left">
                             <b className="font-bold">Donativos en especie:</b>
                             <br />
--- gxla/components/ui/custom-card.tsx
+++ gxla/components/ui/custom-card.tsx
@@ -4,7 +4,7 @@ import type React from "react"
 
 interface CustomCardProps {
   children?: React.ReactNode
-  variant?: "normal" | "big"
+  variant?: "normal" | "big" | "vertical"
   size?: "sm" | "md" | "lg"
   className?: string
   icon?: string
@@ -26,8 +26,8 @@ interface CustomCardProps {
 
 const sizeClasses = {
   sm: "px-4 py-2 text-sm",
-  md: "lg:h-[354px] lg:w-[288px]",
-  lg: "lg:w-[545px] w-7/11 h-9/12",
+  md: "lg:h-[11/12] lg:w-[288px]",
+  lg: "lg:w-[545px] w-9/11 h-9/12",
 }
 
 export function CustomCard({
@@ -54,12 +54,28 @@ export function CustomCard({
   if (variant === "normal") {
     return (
       <div
-        className={`flex flex-col items-center text-center shadow-md rounded-[10px] border border-gray-200 bg-white ${sizeClasses[size]} ${className}`}
+        className={`flex flex-col lg:items-center items-start lg:text-center text-left p-5 shadow-md rounded-[10px] border border-gray-200 bg-white ${sizeClasses[size]} ${className}`}
         onClick={onClick}
       >
-        {icon && <img src={icon} alt="" className="mb-2 w-16 h-16 mt-3" />}
-        {title && <h3 className="lg:text-[32px] text-[24px] font-bold">{title}</h3>}
-        {children && <div className="lg:text-[20px] w-11/12 text-[14px] pt-[20px] font-light mb-5">{children}</div>}
+        <div className="flex lg:flex-col flex-row lg:justify-center items-center">
+          {icon && <img src={icon} alt="" className="lg:mb-2 w-16 h-16 lg:mt-3" />}
+          {title && <h3 className="lg:text-[32px] text-[24px] font-bold">{title}</h3>}
+        </div>
+        {children && <div className="lg:text-[20px] w-11/12 text-[16px] pt-[10px] font-light mb-5">{children}</div>}
+      </div>
+    )
+  }
+  else if(variant === "vertical"){
+    return (
+      <div
+        className={`flex flex-col items-center lg:text-center p-5 shadow-md rounded-[10px] border border-gray-200 bg-white  ${sizeClasses[size]} ${className}`}
+        onClick={onClick}
+      >
+        <div className="flex flex-col lg:justify-center items-center">
+          {icon && <img src={icon} alt="" className="lg:mb-2 w-16 h-16 lg:mt-3" />}
+          {title && <h3 className="lg:text-[32px] text-[24px] font-bold text-center">{title}</h3>}
+        </div>
+        {children && <div className="lg:text-[20px] w-11/12 text-[16px] pt-[10px] font-light mb-5 text-center">{children}</div>}
       </div>
     )
   }
@@ -69,30 +85,53 @@ export function CustomCard({
       className={`flex flex-col items-center justify-center text-center shadow-md rounded-[10px] border border-gray-200 bg-white w-10/12 ${className}`}
       onClick={onClick}
     >
-      {icon && <img src={icon} alt="" className="mb-2 w-20 h-20 mt-3" />}
-      {title && <h3 className="text-[36px] font-bold">{title}</h3>} 
-      <div className="w-10/12 flex flex-col items-center justify-center text-center">
-        <div className="grid lg:grid-cols-3 grid-cols-2 w-full">
-          {title1 && <h1 className="pt-[20px] font-bold text-[32px]">{title1}</h1>}
-          {title2 && <h1 className="pt-[20px] font-bold text-[32px]">{title2}</h1>}
-          {title3 && <h1 className="pt-[20px] font-bold text-[32px]">{title3}</h1>}
-        </div>
-        <div className="grid lg:grid-cols-3 grid-cols-2 w-full">
-          {text1 && <p className="text-[20px] pt-[20px] font-light">{text1}</p>}
-          {text2 && <p className="text-[20px] pt-[20px] font-light">{text2}</p>}
-          {text3 && <p className="text-[20px] pt-[20px] font-light">{text3}</p>}
-        </div>
-        <div className="grid lg:grid-cols-3 grid-cols-2 w-full">
-          {title4 && <h1 className="text-[32px] pt-[20px] font-bold">{title4}</h1>}
-          {title5 && <h1 className="text-[32px] pt-[20px] font-bold">{title5}</h1>}
-          {title6 && <h1 className="text-[32px] pt-[20px] font-bold">{title6}</h1>}
-        </div>
-        <div className="grid lg:grid-cols-3 grid-cols-2 w-full mb-10">
-          {text4 && <p className="text-[20px] pt-[20px] font-light">{text4}</p>}
-          {text5 && <p className="text-[20px] pt-[20px] font-light">{text5}</p>}
-          {text6 && <p className="text-[20px] pt-[20px] font-light">{text6}</p>}
-        </div>
+      {icon && <img src={icon} alt="" className="hidden lg:block mb-2 w-20 h-20" />}
+      {title && <h3 className="lg:text-[36px] text-[24px] font-bold">{title}</h3>}
+      <div className="w-10/12 grid lg:grid-cols-3 grid-cols-2 gap-y-6 gap-x-4 mt-6 mb-10 text-center">
+        {/* 1 */}
+        {title1 && (
+          <div>
+            <h1 className="font-bold lg:text-[28px] text-[18px]">{title1}</h1>
+            {text1 && <p className="lg:text-[18px] text-[16px] pt-2 font-light">{text1}</p>}
+          </div>
+        )}
+        {/* 2 */}
+        {title2 && (
+          <div>
+            <h1 className="font-bold lg:text-[28px] text-[18px]">{title2}</h1>
+            {text2 && <p className="lg:text-[18px] text-[16px] pt-2 font-light">{text2}</p>}
+          </div>
+        )}
+        {/* 3 */}
+        {title3 && (
+          <div>
+            <h1 className="font-bold lg:text-[28px] text-[18px]">{title3}</h1>
+            {text3 && <p className="lg:text-[18px] text-[16px] pt-2 font-light">{text3}</p>}
+          </div>
+        )}
+        {/* 4 */}
+        {title4 && (
+          <div>
+            <h1 className="font-bold lg:text-[28px] text-[18px]">{title4}</h1>
+            {text4 && <p className="lg:text-[18px] text-[16px] pt-2 font-light">{text4}</p>}
+          </div>
+        )}
+        {/* 5 */}
+        {title5 && (
+          <div>
+            <h1 className="font-bold lg:text-[28px] text-[18px]">{title5}</h1>
+            {text5 && <p className="lg:text-[18px] text-[16px] pt-2 font-light">{text5}</p>}
+          </div>
+        )}
+        {/* 6 */}
+        {title6 && (
+          <div>
+            <h1 className="font-bold lg:text-[28px] text-[18px]">{title6}</h1>
+            {text6 && <p className="lg:text-[18px] text-[16px] pt-2 font-light">{text6}</p>}
+          </div>
+        )}
       </div>
+
     </div>
   )
 }
